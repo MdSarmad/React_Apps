4495e554091215bb0f62c5deba3ff555fcbb807f
MdSarmad <28mdsarmad99@gmail.com>
2021-11-29 21:14:03 +0530

    login-page created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Counter from './components/Counter';
 import DisplayName from './components/DisplayName';
+import SignIn from './components/signin/SignIn';
 
 function App(){
   
   return(
     <Router>
       <Switch>
+        <Route exact path = '/signin' component = {SignIn}/>
         <Route exact path = '/' component = {Counter}/>
         <Route exact path = '/display-name' component={DisplayName}/>
       </Switch>
